23ce655edb5f5c38cdd09e2fe28781cbf6bd001b
Giorgio Borelli <casaborelli@gmail.com>
2012-08-12 16:48:40 +0200

    changed pipeline to import test content

--- src/simplemanagement/policy/__init__.py
+++ src/simplemanagement/policy/__init__.py
@@ -1,4 +1,47 @@
+from DateTime import DateTime
 from zope.i18nmessageid import MessageFactory
 
 # Set up the i18n message factory for our package
 MessageFactory = MessageFactory('simplemanagement.policy')
+
+
+def set_operatives(item):
+    from abstract.simplemanagement.structures import Resource
+    data = eval(item)
+
+    def create_operative(val):
+        res = Resource()
+        res.role = val[0]
+        res.user_id = val[1]
+        return res
+
+    return [create_operative(i) for i in data]
+
+
+def set_environments(item):
+    from abstract.simplemanagement.structures import Environment
+    data = eval(item)
+
+    def create_obj(val):
+        res = Environment()
+        res.name = val[0]
+        res.env_type = val[1]
+        res.url = val[2]
+
+        return res
+
+    return [create_obj(i) for i in data]
+
+
+def set_milestones(item):
+    from abstract.simplemanagement.structures import Milestone
+    data = eval(item)
+
+    def create_obj(val):
+        res = Milestone()
+        res.name = val[0]
+        res.status = val[1]
+
+        return res
+
+    return [create_obj(i) for i in data]
