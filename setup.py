1651053579313ecf27af746ef58e75a4dde203fd
Giorgio Borelli <casaborelli@gmail.com>
2012-08-12 16:51:55 +0200

    added plone.app.toolbar and collective.quickupload

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(name='simplemanagement.policy',
       description="Simple management buildout and boilerplate for testing",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
@@ -31,9 +30,11 @@ setup(name='simplemanagement.policy',
           'setuptools',
           'Products.CMFCore',
           'abstract.simplemanagement',
+          'plone.app.toolbar',
+          'collective.quickupload',
           # -*- loadcontent requirements: -*-
           'plone.app.transmogrifier',
-          # 'transmogrify.dexterity',
+          'transmogrify.dexterity',
           # -*- suggested requirements: -*-
           # 'collective.portletpage',
           'collective.contentleadimage',
