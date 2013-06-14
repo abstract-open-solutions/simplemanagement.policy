1c77e5c04e94f1a322076a86cdc0cb5021d93b2d
Simone Deponti <simone.deponti@gmail.com>
2013-06-14 11:52:44 +0200

    Disable KSS Javascript.
    `kss-bbb.js` kept popping up errors on inline validation
    in the Javascript console.
    It did not affect any other Javascript, but no errors is better.

---
+++ src/simplemanagement/policy/upgrades.py
@@ -0,0 +1,20 @@
+import logging
+
+
+# pylint: disable=W0613
+
+
+LOGGER = 'simplemanagement.policy'
+DEFAULT_PROFILE = 'profile-simplemanagement.policy:default'
+
+
+def getLogger(logger=None):
+    if logger is None:
+        logger = logging.getLogger(LOGGER)
+    return logger
+
+
+def upgrade_to_1001(context, logger=None):
+    logger = getLogger(logger)
+    logger.info("Fixing portal_javascripts")
+    context.runImportStepFromProfile(DEFAULT_PROFILE, 'jsregistry')
