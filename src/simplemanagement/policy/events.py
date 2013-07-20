6ad42311e5b917c66f2a3bd4bde2332225fcb533
Maurizio Delmonte <maurizio.delmonte@abstract.it>
2013-07-20 13:34:00 +0200

    refactored reporting story into generic reporting behavior.

--- src/simplemanagement/policy/events.py
+++ src/simplemanagement/policy/events.py
@@ -1,20 +1,45 @@
+from datetime import date
+from datetime import timedelta
 from decimal import Decimal
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.WorkflowCore import WorkflowException
+from .interfaces import IGenericReportingConfig
 from . import MessageFactory as _
 
 
-REPORTING_STORY_ID = 'reporting'
+REPORTING_ITERATION_ID = 'reporting-iteration'
+REPORTING_STORY_ID = 'reporting-story'
 
 
-def create_reporting_story(obj, __):
-    """Creates a story whose only use is to collect bookings
+def create_generic_reporting_ui(obj, __):
+    """Creates an iteration+story whose only use is to collect bookings
     (for projects that have an operative part that can't be tracked).
     """
+    if not IGenericReportingConfig.providedBy(obj):
+        return
+    if not getattr(obj,'activate_generic_reporting', False):
+        return
     _t = obj.translate
-    if REPORTING_STORY_ID not in obj:
-        obj.invokeFactory('Story', REPORTING_STORY_ID)
-        story = obj[REPORTING_STORY_ID]
+    if REPORTING_ITERATION_ID not in obj:
+        obj.invokeFactory('Iteration', REPORTING_ITERATION_ID)
+        iteration = obj[REPORTING_ITERATION_ID]
+        iteration.title = _t(_(u"Reporting"))
+        iteration.description = _t(_(u"Support iteration for generic reporting"))
+        iteration.text = _t(_(u"Used to book generic activities on project. "))
+        iteration.start = date.today()
+        iteration.end = iteration.start + timedelta(180)
+        # XXX: do we need to set proper start/end date?
+        iteration.reindexObject()
+        workflowTool = getToolByName(iteration, "portal_workflow")
+        try:
+            workflowTool.doActionFor(iteration, "activate")
+        except WorkflowException:
+            pass
+    else:
+        iteration = obj.get(REPORTING_ITERATION_ID)
+    if REPORTING_STORY_ID not in iteration:
+        iteration.invokeFactory('Story', REPORTING_STORY_ID)
+        story = iteration[REPORTING_STORY_ID]
         story.title = _t(_(u"Reporting"))
         story.description = _t(_(u"Support content for reporting"))
         story.text = _t(_(u"Here is where bookings must be inserted "
@@ -29,7 +54,8 @@ def create_reporting_story(obj, __):
                           u"or external systems)\n\n"
                           u"*Do not* insert bookings here if:\n\n"
                           u" - There are open iterations and stories\n\n"))
-        obj.estimate = Decimal("0.0")
+        # XXX: check if obj is correct, and not story
+        # obj.estimate = Decimal("0.0")
         assigned_to = story.assigned_to if story.assigned_to is not None else []
         operatives = obj.operatives if obj.operatives is not None else []
         for operative in operatives:
---
+++ src/simplemanagement/policy/interfaces.py
@@ -0,0 +1,22 @@
+from zope.interface import alsoProvides
+from zope import schema
+from z3c.form.browser.checkbox import CheckBoxFieldWidget
+
+from plone.directives import form
+
+from . import MessageFactory as _
+
+
+class IGenericReportingConfig(form.Schema):
+
+    activate_generic_reporting = schema.Bool(
+        title=_(u"Activate Generic Reporting"),
+        description=_(u'check this to create an iteration/story'
+                      u' for generic booking'),
+        default=True,
+    )
+
+    # form.widget(activate_generic_reporting=CheckBoxFieldWidget)
+
+
+alsoProvides(IGenericReportingConfig, form.IFormFieldProvider)
